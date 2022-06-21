27f191d2db3208e463ab4e7f70b0cd8559ca975c
Devin <Devin2152c@gmail.com>
2022-06-21 18:38:59 -0500

    Initial commit

---
+++ app/src/main/java/android/teaching/teaching/MainActivity.java
@@ -0,0 +1,26 @@
+package android.teaching.teaching;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] mobileArray = {"Unit 1","Unit 2","Unit 3","Unit 4",
+            "Unit 5","Unit 6","Unit 7","Unit 8", "Unit 9", "Unit 10"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_listview, mobileArray);
+
+        ListView listView = (ListView) findViewById(R.id.mobile_list);
+        listView.setAdapter(adapter);
+    }
+
+
+}
